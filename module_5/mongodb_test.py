b1bf27dfa6542fd474f2675167119e9b9e34d2f5
DSchleede72 <133394426+DSchleede72@users.noreply.github.com>
2023-07-06 20:51:59 -0400

    Delete module_5 directory

--- module_5/mongodb_test.py
+++
@@ -1,9 +0,0 @@
-#import pymongo from MongoClient
-from pymongo import MongoClient
-#assign connection string to variable url
-url = "mongodb+srv://admin:admin@cluster0.8do7jnn.mongodb.net/?retryWrites=true&w=majority"
-#create a variable named client
-client = MongoClient(url)
-#create a variable named db
-db = client.pytech
-print(db.list_collection_names)     
